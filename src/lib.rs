30a68deecba5fe0b24f420a79b8403fe77ea916a
justkinou <justkinou@proton.me>
2026-09-14 00:30:54 +0200

    feat: expose grpc api of identity service

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use crate::database::IdentityDb;
+use tonic::transport::Server;
+
+use crate::{avatar_storage::AvatarStorage, database::IdentityDb, service::{IdentityService, identity::identity_server::IdentityServer}};
 
 pub mod config;
 mod database;
@@ -7,11 +9,21 @@ mod avatar_storage;
 mod service;
 
 pub async fn run(conf: config::Config) -> anyhow::Result<()> {
-    println!("conf: {:?}", conf);
+    let addr = "0.0.0.0:8080".parse()?;
 
     let identity_db = IdentityDb::build(&conf.postgres)
         .await
         .expect("failed to connect to database");
+    let avatar_storage = AvatarStorage::build(&conf.rustfs)
+        .await
+        .expect("failed to connect to avatar storage");
+
+    let identity_service = IdentityService::new(identity_db, avatar_storage);
+
+    Server::builder()
+        .add_service(IdentityServer::new(identity_service))
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
